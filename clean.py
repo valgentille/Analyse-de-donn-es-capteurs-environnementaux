62f70b03c4ea66b1d75ee1fb270a0b121b199beb
valgentille <valerieguineo@gmail.com>
2026-05-07 03:45:52 +0100

    le dataset weatherhistorymarche

--- clean.py
+++ clean.py
@@ -2,21 +2,26 @@ import pandas as pd
 import numpy as np
 import io
 
+
+COLUMN_MAP = {
+    "Temperature (C)": "temperature",
+    "Humidity": "humidity",
+    "Formatted Date": "Formatted Date",
+}
+
 def load_data(uploaded_file):
-    # Lire les bytes et les passer à pandas
-    uploaded_file.seek(0) 
+    uploaded_file.seek(0)
     bytes_data = uploaded_file.read()
     df = pd.read_csv(io.BytesIO(bytes_data), parse_dates=["Formatted Date"])
+
+   
+    df = df.rename(columns=COLUMN_MAP)
+
     return df
 
 def clean_data(df):
-    # 1. Supprimer les doublons
     df = df.drop_duplicates()
-    
-    # 2. Gérer les valeurs manquantes
     df = df.interpolate(method="linear")
-    
-    # 3. Détecter les valeurs aberrantes (température impossible)
     df = df[df["temperature"].between(-20, 50)]
-    
+
     return df
